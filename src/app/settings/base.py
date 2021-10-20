6e174cadb60c91bdeb2dde36eef3ef3a0fa093f8
Viktor.Vasilev <Viktor.Vasilev@x5.ru>
2021-10-21 02:24:05 +0300

    Async-API-sprint-2-prepare-tests-environment prepare docker before tests initialize

--- src/app/settings/base.py
+++ src/app/settings/base.py
@@ -71,8 +71,8 @@ class CommonSettings(BaseSettings):
 
     DEBUG: bool = False
     LOG_LEVEL: str = "INFO"
-    SHARED_DIR: str = "/app/shared"
-    DIR_LOGS: Path = Path(SHARED_DIR, "/app/shared/logs")
+    SHARED_DIR: str = "/code/shared"
+    DIR_LOGS: Path = Path(SHARED_DIR, "/code/shared/logs")
 
     VALID_SORTING_FIELDS: list[str] = ["rating"]
 
