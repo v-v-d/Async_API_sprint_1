d0a7338856a4676df871814ea0d09bf07fec5e6a
karnet <e.abramovicj@icode.by>
2021-10-25 14:47:39 +0200

    Test for genres

--- src/tests/functional/src/api/v1/conftest.py
+++ src/tests/functional/src/api/v1/conftest.py
@@ -5,6 +5,8 @@ import pytest
 from app.services.base import BaseServiceError, NotFoundError
 from app.services.films import main
 from app.services.films.main import FilmsService
+from app.services.genres import main
+from app.services.genres.main import GenreService
 
 
 @pytest.fixture
@@ -28,3 +30,26 @@ async def mocked_es_unexpected_exception(monkeypatch):
 async def mocked_es_not_found_error(monkeypatch):
     monkeypatch.setattr(FilmsService, "_request_elastic", AsyncMock())
     main.FilmsService._request_elastic.side_effect = NotFoundError  # noqa
+
+
+@pytest.fixture
+async def v1_genres_url():
+    return "/api/v1/genres"
+
+
+@pytest.fixture
+async def mocked_es_genre_invalid_response(monkeypatch):
+    monkeypatch.setattr(GenreService, "_request_elastic", AsyncMock())
+    main.GenreService._request_elastic.return_value = {}  # noqa
+
+
+@pytest.fixture
+async def mocked_es_genre_unexpected_exception(monkeypatch):
+    monkeypatch.setattr(GenreService, "_request_elastic", AsyncMock())
+    main.GenreService._request_elastic.side_effect = BaseServiceError  # noqa
+
+
+@pytest.fixture
+async def mocked_es_genre_not_found_error(monkeypatch):
+    monkeypatch.setattr(GenreService, "_request_elastic", AsyncMock())
+    main.GenreService._request_elastic.side_effect = NotFoundError  # noqa
